8669e122fd1cfa8b70af540a36f6cd04018c5278
Gabriel Alves <gabriel.s.t.a.r.k@hotmail.com>
2022-05-17 22:01:37 -0300

    TrasactionsTable componente

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,9 +1,11 @@
 import { Container } from "./style";
 import { Summary } from '../Summary'
+import { TransactionsTable } from "../TransactionsTable";
 export function Dashboard(){
   return(
     <Container>
       <Summary/>
+      <TransactionsTable/>
     </Container>
   );
 }
